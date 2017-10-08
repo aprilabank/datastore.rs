e5218be3f86badceecd5a8a2cc5adb2fb93a46ae
Vincent Ambo <tazjin@gmail.com>
2017-10-08 17:50:27 +0200

    feat(serde_ds): Expand serialisation error type
    Adds variants to the error type that cover the following problems:
    
    * a value is no sanely serialisable into a Datastore type (there are a few of these!)
    * a compound type is not sanely serialisable (not yet sure if this is actually needed)
    * custom errors created in Serialise / Deserialies implementations

--- src/serde_ds/error.rs
+++ src/serde_ds/error.rs
@@ -7,20 +7,24 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Error {
-    SomeError()
+    SomeError(),
+    SerialisationError(String),
+    DeserialisationError(String),
+    UnsupportedValueType(&'static str),
+    UnsupportedCompoundType(&'static str),
 }
 
 // https://serde.rs/error-handling.html
 
 impl ser::Error for Error {
     fn custom<T: Display> (msg: T) -> Self {
-        unimplemented!()
+        Error::SerialisationError(format!("{}", msg))
     }
 }
 
 impl de::Error for Error {
     fn custom<T: Display> (msg: T) -> Self {
-        unimplemented!()
+        Error::DeserialisationError(format!("{}", msg))
     }
 }
 
