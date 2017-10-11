dd41dbc6383b97094e64be7466e215de356a9f0c
Vincent Ambo <tazjin@gmail.com>
2017-10-11 12:24:34 +0200

    feat(serde_ds): Implement error descriptions
    Implements the traits for describing and displaying serialisation errors.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate serde_derive;
 
+#[cfg(test)]
 #[macro_use]
 extern crate maplit;
 
--- src/serde_ds/error.rs
+++ src/serde_ds/error.rs
@@ -8,11 +8,10 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Error {
-    SerialisationError(String),
-    DeserialisationError(String),
+    SerializationError(String),
+    DeserializationError(String),
     ParseIntError(),
     UnsupportedValueType(&'static str),
-    UnsupportedCompoundType(&'static str),
     UnsupportedKeyType(),
     NonSelfDescribingType(),
     ExpectedType(&'static str),
@@ -28,24 +27,62 @@ impl From<num::ParseIntError> for Error {
 
 impl ser::Error for Error {
     fn custom<T: Display>(msg: T) -> Self {
-        Error::SerialisationError(format!("{}", msg))
+        Error::SerializationError(format!("{}", msg))
     }
 }
 
 impl de::Error for Error {
     fn custom<T: Display>(msg: T) -> Self {
-        Error::DeserialisationError(format!("{}", msg))
+        Error::DeserializationError(format!("{}", msg))
     }
 }
 
 impl Display for Error {
-    fn fmt(&self, _f: &mut fmt::Formatter) -> fmt::Result {
-        unimplemented!()
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        // Import the 'Error' trait locally scoped to prevent naming conflicts and access the
+        // 'description' method.
+        use std::error::Error;
+        use serde_ds::Error::*;
+
+        // Some errors carry extra information that may be relevant when 'Display' is called on
+        // the error. This extends the simple error descriptions where appropriate.
+        match *self {
+            SerializationError(ref msg) =>
+                fmt.write_fmt(format_args!("{}: {}", self.description(), msg)),
+            DeserializationError(ref msg) =>
+                fmt.write_fmt(format_args!("{}: {}", self.description(), msg)),
+            NotYetImplemented(ref t) =>
+                fmt.write_fmt(format_args!("{}: {}", self.description(), t)),
+            ExpectedType(t) =>
+                fmt.write_fmt(format_args!("{}: expected {}", self.description(), t)),
+            UnsupportedValueType(t) =>
+                fmt.write_fmt(format_args!("{}: {}", self.description(), t)),
+            _ => fmt.write_str(self.description())
+        }
     }
 }
 
 impl std::error::Error for Error {
     fn description(&self) -> &str {
-        unimplemented!()
+        match *self {
+            Error::ParseIntError() =>
+                "could not parse integer from value",
+            Error::NonSelfDescribingType() =>
+                "cannot automatically determine desired type representation",
+            Error::DoubleSizeMismatch() =>
+                "floating-point value too large for chosen type",
+            Error::SerializationError(_) =>
+                "error during serialization",
+            Error::DeserializationError(_) =>
+                "error during deserialization",
+            Error::NotYetImplemented(_) =>
+                "support for type not yet implemented",
+            Error::ExpectedType(_) =>
+                "unexpected type encountered",
+            Error::UnsupportedValueType(_) =>
+                "unsupported value type",
+            Error::UnsupportedKeyType() =>
+                "non-string key types are unsupported"
+        }
     }
 }
